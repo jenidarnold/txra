4a4c8be93e655dd8ea425c43caef11c2d76501ef
Julienne Arnold <julie.enid@gmail.com>
2017-06-18 11:15:23 -0500

    seed new users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -31,6 +31,32 @@ class UsersTableSeeder extends Seeder
 			'email' => 'guest@txra.org',
 			'password' => Hash::make('guest1234'),
 		]);
+
+		User::create([
+			'id' => '31198',
+			'first_name' => 'Brad',
+			'last_name' => 'Giezentanner',
+			'middle_name' => '',
+			'email' => 'brad@txra.org',
+			'password' => Hash::make('board'),
+		]);
+
+		User::create([
+			'id' => '140318',
+			'first_name' => 'Dale',
+			'last_name' => 'Gosser',
+			'middle_name' => '',
+			'email' => 'dale@txra.org',
+			'password' => Hash::make('board'),
+		]);
 		
+		User::create([
+			'id' => '170599',
+			'first_name' => 'Mike',
+			'last_name' => 'Sorenson',
+			'middle_name' => '',
+			'email' => 'sorenson@txra.org',
+			'password' => Hash::make('board'),
+		]);
     }
 }
