00a50f76809e4e7c7ca2428e9457e0aaa3ec6438
Julienne Arnold <julie.enid@gmail.com>
2017-07-16 14:34:13 -0500

    fix Blog Seeder Class name

--- database/seeds/BlogTableSeeder.php
+++ database/seeds/BlogTableSeeder.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Seeder;
 //use Illuminate\Database\Eloquent\Model;
 //use App\Blog;
 
-class InstructorsTableSeeder extends Seeder {
+class BlogTableSeeder extends Seeder {
 	/**
 	 * Run the database seeds.
 	 *
