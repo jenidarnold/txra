36c6b0abf8b60bbf792e3be4bad7d5340b592b69
Julienne Arnold <julie.enid@gmail.com>
2017-08-17 18:52:20 -0500

    bind modular ranks

--- app/Http/Controllers/Members/RankingsController.php
+++ app/Http/Controllers/Members/RankingsController.php
@@ -30,37 +30,46 @@ class RankingsController extends Controller {
 	public function index(Request $request)
 	{
 
-		//Texas Mens 
-		$tms =  Rank::where('group_id', '=','1')->orderBy('rank');
-		$featured = Rank::inRandomOrder()->where('group_id', '=','1')->first();
-		$tms = $tms->limit(10)->get();
-		$tms->featured = $featured;
+		// //Texas Mens 
+		// $tms =  Rank::where('group_id', '=','1')->orderBy('rank');
+		// $featured = Rank::inRandomOrder()->where('group_id', '=','1')->first();
+		// $tms = $tms->limit(10)->get();
+		// $tms->featured = $featured;
 
-		$tmd = Rank::all();
-		$tmx = Rank::all();
+		//Get Ranks
+		$tms = $this->get_rank_by_group(1,1);
+		$tws =  $this->get_rank_by_group(2,1);
+		$tmd = $this->get_rank_by_group(3,1);
+		$twd =  $this->get_rank_by_group(4,1);
+		$tmx = $this->get_rank_by_group(5,1);
+		$twx =  $this->get_rank_by_group(6,1);
+
+		$nms = $this->get_rank_by_group(1,0);
+		$nws =  $this->get_rank_by_group(2,0);
+		$nmd = $this->get_rank_by_group(3,0);
+		$nwd =  $this->get_rank_by_group(4,0);
+		$nmx = $this->get_rank_by_group(5,0);
+		$nwx =  $this->get_rank_by_group(6,0);
+
+		//Set titles
+		$tms->title = "TX Men's Singles";
+		$tws->title = "TX Women's Singles";
+		$tmd->title = "TX Men's Doubles";
+		$twd->title = "TX Women's Doubles";
+		$tmx->title = "TX Men's Mixed";
+		$twx->title = "TX Women's Mixed";
+
+		//Set Filters
+		$tms->filter = "TX singles men";
+		$tws->filter = "TX singles women";
+		$tmd->filter = "TX doubles men";
+		$twd->filter = "TX dobules women";
+		$tmx->filter = "TX mixed men";
+		$twx->filter = "TX mixed women";
 		
-		//Texas Womens 		
-		$tws =  Rank::where('group_id', '=','2')->orderBy('rank');
-		$featured = Rank::inRandomOrder()->where('group_id', '=','2')->first();
-		$tws = $tws->limit(10)->get();
-		$tws->featured = $featured;
-
-
-		$twd = Rank::all();
-		$twx = Rank::all();
-
-		//National Mens 
-		$nms = Rank::all();
-		$nmd = Rank::all();
-		$nmx = Rank::all();
 		
-		//National Womens
-		$nws = Rank::all();
-		$nwd = Rank::all();
-		$nwx = Rank::all();
-
-
-		$tms->url ="http://www.usaracquetballevents.com/rankings.asp?sortOptions=YES&sex=M&stateID=1";
+		//Set urls
+		$tms->url ="http://www.usaracquetballevents.com/rankings.asp?sortOptions=YES&sex=M&stateID=1";		
 		$tmd->url ="http://www.usaracquetballevents.com/rankings.asp?sortOptions=YES&sex=MD&stateID=1";
 		$tmx->url ="http://www.usaracquetballevents.com/rankings.asp?sortOptions=YES&sex=MX&stateID=1";
 
@@ -77,15 +86,54 @@ class RankingsController extends Controller {
 		$nwx->url ="http://www.usaracquetballevents.com/rankings.asp?sortOptions=YES&sex=WX&stateID=0";
 
 		//need to filter by top effective date
-		
+		//need to push only if not empty
+		$ranks = array();
+		if($tms->count() > 0){
+			array_push($ranks, $tms);
+		}
+		if($tws->count() > 0){		
+			array_push($ranks, $tws);	
+		}
+		if($tmd->count() > 0){
+			array_push($ranks, $tmd);
+		}
+		if($twd->count() > 0){
+			array_push($ranks, $twd);		
+		}
+		if($tmx->count() > 0){
+			array_push($ranks, $tmx);
+		}
+		if($twx->count() > 0){
+			array_push($ranks, $twx);	
+		}
 
 
-
-
-
-		return view('members/rankings/index', compact('tms', 'tws'));
+		return view('members/rankings/index', compact('ranks'));
 	}
 
+	/*
+	
+	 */
+	public function get_rank_by_group($group_id, $location_id){
+
+		$rank =  Rank::where('group_id', '=',$group_id)
+				->where('location_id','=', $location_id)
+				->orderBy('rank');
+
+		$rand = \DB::table('ranks')
+			->join('usar_members', 'ranks.usar_id', '=', 'usar_members.id')
+			->where('group_id', '=',$group_id)
+			->where('location_id','=', $location_id)			
+        	->orderBy(\DB::raw('RAND()'))
+        	->take(1)
+			->first();
+
+
+		$rank = $rank->limit(10)->get();
+		$rank->featured = $rand;
+
+		return $rank;
+	}
 		
 	/**
 	 * Display member rankings.
@@ -111,11 +159,11 @@ class RankingsController extends Controller {
 		$maxRank = 10;
 
 		$ss = new Scraper();
-		$tournaments = $ss->get_rankings($group_id, $location_id, $maxRank );
+		$rankings = $ss->get_rankings($group_id, $location_id, $maxRank );
 
-		$rankings = Rank::all();
+		//$rankings = Rank::all();
 
-		return view('members/rankings', compact('$rankings', 'group'));
+		dd($rankings);
 	}
 
 }
