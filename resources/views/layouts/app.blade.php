da4f9d9df7c4d44c0248d6ab24a8aad3af067367
Julienne Arnold <julie.enid@gmail.com>
2017-09-20 12:49:27 -0500

    add apple-icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,10 @@
 		<link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
 
 		<!-- Mobilie icons -->
-		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ assest('images/icons/txra-icon-57x57.png')}}">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ assest('images/icons/txra-icon-72x72.png')}}">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ assest('images/icons/txra-icon-114x114.png')}}">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ assest('images/icons/txra-icon-144x144.png')}}">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('images/icons/txra-icon-57x57.png')}}">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('images/icons/txra-icon-72x72.png')}}">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('images/icons/txra-icon-114x114.png')}}">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('images/icons/txra-icon-144x144.png')}}">
 
 		@yield('head')
 
