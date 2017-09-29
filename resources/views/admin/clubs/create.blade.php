3cd8fac73c519d7a48453e62fe3046e5612e496b
Julienne Arnold <julie.enid@gmail.com>
2017-09-28 20:13:21 -0500

    admin create club

--- resources/views/admin/clubs/create.blade.php
+++ resources/views/admin/clubs/create.blade.php
@@ -32,21 +32,68 @@
 	            <form action="{{route('admin.clubs.store')}}" method="post" enctype="multipart/form-data">
 	            	{{ csrf_field() }}		
 
-	            	<div class="row">
-
-						<div class="form-group">
-							<label class="control-label">First Name</label>
-							<input type="text" name="first_name" value="{{ Input::old('first_name')}}" class="form-control">
+	            	<div class="row">						
+						<div class="col-sm-12">
+							<label class="control-label">Club Name</label>
+							<input type="text" name="name" value="{{ Input::old('name')}}" class="form-control">
 						</div>
-						<div class="form-group">
-							<label class="control-label">Last Name</label>
-							<input type="text" name="last_name" value="{{Input::old('last_name')}}" class="form-control">
+						<div class="col-sm-12">
+							<label class="control-label">Address</label>
+							<input type="text" name="address" value="{{Input::old('address')}}" class="form-control">
 						</div>	
 					</div>
+					<div class="row">							
+						<div class="col-sm-6">
+							<label class="control-label">City</label>							
+							<input type="text" name="city" value="{{Input::old('city')}}" class="form-control">
+						</div>
+					
+						<div class="col-sm-3">
+							<label class="control-label">State</label>
+							<input type="text" name="state" value="{{Input::old('state')}}" class="form-control">
+						</div>
+					
+						<div class="col-sm-3">
+							<label class="control-label">Zip</label>
+							<input type="text" name="zip" value="{{Input::old('zip')}}" class="form-control">
+						</div>
+					</div>
+					<div class="row">							
+						<div class="col-sm-3">
+							<label class="control-label">Phone</label>							
+							<input type="text" name="phone" value="{{Input::old('phone')}}" class="form-control">
+						</div>
+						<div class="col-sm-3">
+							<label class="control-label">Type</label>							
+							<input type="text" name="type" value="{{Input::old('type')}}" class="form-control">
+						</div>
+					
+						<div class="col-sm-3">
+							<label class="control-label"># Courts</label>							
+							<input type="text" name="courts" value="{{Input::old('courts')}}" class="form-control">
+						</div>
+					</div>
 					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Email</label>
-							<input type="text" name="email" value="{{Input::old('email')}}" class="form-control">
+						<div class="col-sm-3">
+							<label class="control-label">Lat</label>							
+							<input type="text" name="lat" value="{{Input::old('lat')}}" class="form-control">
+						</div>
+					
+						<div class="col-sm-3">
+							<label class="control-label">Lng</label>							
+							<input type="text" name="lng" value="{{Input::old('lng')}}" class="form-control">
+						</div>
+					</div>			
+					<div class="row">
+						<div class="col-sm-12">
+							<label class="control-label">Url</label>							
+							<input type="text" name="url" value="{{Input::old('url')}}" class="form-control">
+						</div>
+					</div>
+					<div class="row">							
+						<div class="col-sm-12">
+							<label class="control-label">Info</label>							
+							<input type="text" name="info" value="{{Input::old('info')}}" class="form-control">
 						</div>
 					</div>
 					<div class="row">
