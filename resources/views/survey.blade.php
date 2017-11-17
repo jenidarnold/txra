298cdfb0ae900d9f0cd20fccdf4aedf59b82c0e5
Julienne Arnold <julie.enid@gmail.com>
2017-11-17 09:01:15 -0600

    survey in iframe

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -9,8 +9,12 @@
 @stop
 @section('content')
 	<center>
+
+	<iframe border=0 src="https://www.surveymonkey.com/r/FC96JJY" width="100%" height="600">
 		
-		<script>(function(t,e,n,c){var o,s,a;t.SMCX=t.SMCX||[],e.getElementById(c)||(o=e.getElementsByTagName(n),s=o[o.length-1],a=e.createElement(n),a.type="text/javascript",a.async=!0,a.id=c,a.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/LxJhR_2BG58aL5TQYAmB0UFgzVQV7bYbnIpCZw0k624VfYf2S6PAF84_2FIxX3YSuchA.js"].join(""),s.parentNode.insertBefore(a,s))})(window,document,"script","smcx-sdk");</script><a style="font: 12px Helvetica, sans-serif; color: #999; text-decoration: none;" href=https://www.surveymonkey.com>  </a>
+
+	</iframe>
+
 
 	</center>
 
